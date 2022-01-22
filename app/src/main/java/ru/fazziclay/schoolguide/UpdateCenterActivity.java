d0bebdf666cd70f75279ad1fbe112f7eb8835761
fazziclay <fazziclay@gmail.com>
2022-01-22 17:23:18 +0300

    update center translated

--- app/src/main/java/ru/fazziclay/schoolguide/UpdateCenterActivity.java
+++ app/src/main/java/ru/fazziclay/schoolguide/UpdateCenterActivity.java
@@ -6,10 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -53,7 +51,7 @@ public class UpdateCenterActivity extends AppCompatActivity {
 
         binding = ActivityUpdateCenterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setTitle("SchoolGuide - Update Center"); // TODO: 2022-01-21 make translatable
+        setTitle(R.string.updatecenter_activityTitle);
 
         new Thread(() -> {
             Status status = load();
@@ -61,27 +59,28 @@ public class UpdateCenterActivity extends AppCompatActivity {
         }).start();
     }
 
-    @SuppressLint("SetTextI18n")
     public void updateUI(Status status) {
         binding.loadingGroup.setVisibility(GONE);
         binding.mainGroup.setVisibility(View.VISIBLE);
         binding.actionButton.setVisibility(GONE);
 
+        if (status.latestVersion == null && status == Status.OUTDATED) {
+            status = Status.ERROR.setException(new IllegalStateException("latestVersion is null"));
+        }
         if (status == Status.ERROR) {
-            // TODO: 2022-01-21 make translatable
-            binding.title.setText("Ошибка :(");
+            binding.title.setText(R.string.updatecenter_error_title);
             if (status.exception instanceof UnknownHostException) {
-                binding.text.setText("$[-#ff2222]Отсутствует подключение к $[@bold]интернету$[@reset]!");
+                binding.text.setText(R.string.updatecenter_error_unknownHost_text);
                 binding.actionButton.setVisibility(View.VISIBLE);
-                binding.actionButton.setText("Перезагрузить");
+                binding.actionButton.setText(R.string.updatecenter_error_unknownHost_reload);
                 binding.actionButton.setOnClickListener(ignore -> {
                     startActivity(new Intent(this, UpdateCenterActivity.class));
                     finish();
                 });
             } else if (status.exception instanceof FileNotFoundException) {
-                binding.text.setText("$[@bold;-#ff0000]Сервис закрыт :($[@reset;-reset]\nПриложение не смогло найти последнюю версию, $[@bold;-#dddddd]очень рекомендуем зайти на страницу проекта$[@reset;-reset]\nhttps://github.com/FazziCLAY/SchoolGuide/");
+                binding.text.setText(R.string.updatecenter_error_fileNotFound_text);
             } else {
-                binding.text.setText(String.format("$[-#ffffff;@bold]Неизвестная ошибка.$[@reset;-reset]\nПриложение не смогло распознать ошибку, скорее всего сейчас технические неполадки\n\nОшибка: $[=#080607;-#cc0000;=#000000] %s", status.exception.toString()));
+                binding.text.setText(getString(R.string.updatecenter_error_generic_text, status.exception.toString()));
             }
             setupColorizeText();
             setupLinkableText();
@@ -89,29 +88,31 @@ public class UpdateCenterActivity extends AppCompatActivity {
         }
 
         if (status == Status.UPDATED) {
-            binding.title.setText("Вы крутой!");
+            binding.title.setText(R.string.updatecenter_updated_title);
             binding.title.setTextColor(Color.GREEN);
-            binding.text.setText("$[-#a0a0ff]Сейчас у вас установлена $[@bold;-#7070ff]самая последняя$[@reset;-#a0a0ff] версия, так держать!");
+            binding.text.setText(R.string.updatecenter_updated_text);
             setupColorizeText();
             return;
         }
 
         if (status == Status.OUTDATED) {
-            binding.title.setText(String.format("Доступно обновление\n%s", status.latestVersion.getName()));
-            binding.text.setText(status.latestVersion.getChangelog() == null ? "" : status.latestVersion.getChangelog()
+            binding.title.setText(getString(R.string.updatecenter_outdated_title, status.latestVersion.getName()));
+            binding.text.setText(status.latestVersion.getChangelog() == null ? getString(R.string.updatecenter_outdated_changelogEmpty_text) : status.latestVersion.getChangelog()
                     .replace("$(CLIENT_VERSION_CODE)", SharedConstrains.APPLICATION_VERSION_CODE+"")
                     .replace("$(CLIENT_VERSION_NAME)", SharedConstrains.APPLICATION_VERSION_NAME)
             );
             setupColorizeText();
             setupLinkableText();
             binding.actionButton.setVisibility(View.VISIBLE);
-            binding.actionButton.setText("Скачать и установить"); // TODO: 2022-01-21 make translatable
+            binding.actionButton.setText(R.string.updatecenter_outdated_openDownloadLink);
+
+            String downloadUrl = status.latestVersion.getDownloadUrl();
             binding.actionButton.setOnClickListener(ignore -> {
                 try {
-                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(status.latestVersion.getDownloadUrl()));
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(downloadUrl));
                     startActivity(browserIntent);
                 } catch (ActivityNotFoundException e) {
-                    Toast.makeText(this, "Пожалуйста установите браузер (например FireFox)", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, R.string.updatecenter_outdated_downloadError_activityNotFound, Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -129,23 +130,24 @@ public class UpdateCenterActivity extends AppCompatActivity {
 
     public Status load() {
         final int debug = -1;
-        if (debug == 0) {
-        } else if (debug == 1) {
-            return Status.ERROR.setException(new UnknownHostException());
-        } else if (debug == 2) {
-            return Status.ERROR.setException(new FileNotFoundException());
-        } else if (debug == 3) {
-            return Status.ERROR.setException(new RuntimeException());
-        } else if (debug == 4) {
-            return Status.UPDATED;
-        } else if (debug == 5) {
-            HashMap<String, String> change = new HashMap<>();
-            change.put("default", "Пофикшены баги:\nКорова прилипала к стене\n$[-#ff0000]Вылет из за краша$[-reset]\n\nПодпишись: https://youtube.com/\n\nПривет $[@italic;-#ff0000;=#00cccc]owoPeef  $[@reset;-reset;=reset]& $[@italic;-#00ff00;=#cc00cc]_Dane4ka_");
-            HashMap<String, String> download = new HashMap<>();
-            download.put("release", "https://google.com");
-            download.put("debug", "https://yandex.ru");
-            Manifest.ManifestVersion v = new Manifest.ManifestVersion(20, "0.6 - ReWriTTen ", change, download);
-            return Status.OUTDATED.setLatestVersion(v);
+        if (debug != 0) {
+            if (debug == 1) {
+                return Status.ERROR.setException(new UnknownHostException());
+            } else if (debug == 2) {
+                return Status.ERROR.setException(new FileNotFoundException());
+            } else if (debug == 3) {
+                return Status.ERROR.setException(new RuntimeException());
+            } else if (debug == 4) {
+                return Status.UPDATED;
+            } else if (debug == 5) {
+                HashMap<String, String> change = new HashMap<>();
+                change.put("default", "Пофикшены баги:\nКорова прилипала к стене\n$[-#ff0000]Вылет из за краша$[-reset]\n\nПодпишись: https://youtube.com/\n\nПривет $[@italic;-#ff0000;=#00cccc]owoPeef  $[@reset;-reset;=reset]& $[@italic;-#00ff00;=#cc00cc]_Dane4ka_");
+                HashMap<String, String> download = new HashMap<>();
+                download.put("release", "https://google.com");
+                download.put("debug", "https://yandex.ru");
+                Manifest.ManifestVersion v = new Manifest.ManifestVersion(20, "0.6 - ReWriTTen ", change, download);
+                return Status.OUTDATED.setLatestVersion(v);
+            }
         }
         try {
             String fileContent = NetworkUtil.parseTextPage(SharedConstrains.VERSION_MANIFEST_V2);
