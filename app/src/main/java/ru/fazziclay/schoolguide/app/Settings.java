5616519e67773aed424b57f1f41aa46e87f85c04
fazziclay <fazziclay@gmail.com>
2022-01-23 17:31:19 +0300

    cleanup

--- app/src/main/java/ru/fazziclay/schoolguide/app/Settings.java
+++ app/src/main/java/ru/fazziclay/schoolguide/app/Settings.java
@@ -2,7 +2,7 @@ package ru.fazziclay.schoolguide.app;
 
 public class Settings {
     public boolean developerFeatures = false;
-    public boolean isScheduleInformatorEnabled = true;
     public int scheduleNotifyBeforeTime = 2*60*60;
     public final boolean stopServiceIsNone = false;
+    public boolean isFirstMonday = true;
 }
