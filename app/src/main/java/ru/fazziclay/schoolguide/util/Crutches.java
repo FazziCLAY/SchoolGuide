cffeb3d4e133cbfeff51504211ffe93eadf052fc
fazziclay <fazziclay@gmail.com>
2022-02-04 23:40:35 +0300

    cleanup

--- app/src/main/java/ru/fazziclay/schoolguide/util/Crutches.java
+++ app/src/main/java/ru/fazziclay/schoolguide/util/Crutches.java
@@ -12,28 +12,4 @@ public class Crutches {
             if (System.currentTimeMillis() - startCrutch > maxWait) break;
         }
     }
-
-    /*1private void sendUpdateNotify() {
-        final int NOTIFICATION_ID = UpdateCenterActivity.NOTIFICATION_ID;
-        final String NOTIFICATION_CHANNEL_ID = UpdateCenterActivity.NOTIFICATION_CHANNEL_ID;
-
-        PendingIntent pendingIntent;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            pendingIntent = PendingIntent.getActivity(this, 0, UpdateCenterActivity.getLaunchIntent(this), PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-        } else {
-            pendingIntent = PendingIntent.getActivity(this, 0, UpdateCenterActivity.getLaunchIntent(this), PendingIntent.FLAG_UPDATE_CURRENT);
-        }
-
-        Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setSound(null)
-                .setOnlyAlertOnce(true)
-                .setSilent(true)
-                .setContentTitle(getString(R.string.updatecenter_notification_title))
-                .setContentText(getString(R.string.updatecenter_notification_text))
-                .setContentIntent(pendingIntent)
-                .build();
-
-        notificationManagerCompat.notify(NOTIFICATION_ID, notification);
-    }*/
 }
