7bcfbf358f90f4337493321bb3e6246e0aa3205a
fazziclay <fazziclay@gmail.com>
2022-02-05 11:23:20 +0300

    private & added auto fix null presets hashmap

--- app/src/main/java/ru/fazziclay/schoolguide/app/scheduleinformator/appschedule/PresetList.java
+++ app/src/main/java/ru/fazziclay/schoolguide/app/scheduleinformator/appschedule/PresetList.java
@@ -7,14 +7,18 @@ import ru.fazziclay.schoolguide.util.SortUtil;
 import ru.fazziclay.schoolguide.util.UUIDUtil;
 
 public class PresetList {
-    public HashMap<UUID, Preset> presets = new HashMap<>();
+    private HashMap<UUID, Preset> presets = new HashMap<>();
 
+    private void fix() {
+        if (presets == null) presets = new HashMap<>();
+    }
 
     /**
      * Выдаёт пресет по его Айди, если нету то null
      * @return Пресет
      * **/
     public Preset getPreset(UUID uuid) {
+        fix();
         return presets.get(uuid);
     }
 
@@ -22,6 +26,7 @@ public class PresetList {
      * Вставляет по айди пресет в список, если под таким ключём уже есть, заменяет
      * **/
     public void putPreset(UUID uuid, Preset preset) {
+        fix();
         presets.put(uuid, preset);
     }
 
@@ -29,6 +34,7 @@ public class PresetList {
      * Добавить пресет в список, возвращает его ID
      * **/
     public UUID addPreset(Preset preset) {
+        fix();
         for (Preset p : presets.values()) {
             if (p == preset) throw new RuntimeException("preset exist in preset list!");
         }
@@ -41,6 +47,7 @@ public class PresetList {
      * Выдаёт список айдишников пресетов, они отсортированы по имени пресета
      * **/
     public UUID[] getPresetsIds() {
+        fix();
         UUID[] ids = presets.keySet().toArray(new UUID[0]);
         SortUtil.sort(ids, uuid -> {
             Preset preset = getPreset(uuid);
@@ -50,12 +57,14 @@ public class PresetList {
     }
 
     public Preset[] getPresets() {
+        fix();
         Preset[] presets = this.presets.values().toArray(new Preset[0]);
         SortUtil.sort(presets, Preset::getName);
         return presets;
     }
 
     public void removePreset(UUID uuid) {
+        fix();
         presets.remove(uuid);
     }
 }
