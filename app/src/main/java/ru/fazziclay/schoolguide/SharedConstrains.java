69d4a756f0e58f6b3dc24fe68bdc23823939668e
fazziclay <fazziclay@gmail.com>
2022-02-05 13:35:09 +0300

    change global links to main branch

--- app/src/main/java/ru/fazziclay/schoolguide/SharedConstrains.java
+++ app/src/main/java/ru/fazziclay/schoolguide/SharedConstrains.java
@@ -26,10 +26,9 @@ public class SharedConstrains {
     public static final String APPLICATION_ID = BuildConfig.APPLICATION_ID;
     public static final String APPLICATION_BUILD_TYPE = BuildConfig.BUILD_TYPE;
 
-    // TODO: 2022-01-26 change to main branch
-    public static final String KEYS_V2 = "https://raw.githubusercontent.com/FazziCLAY/SchoolGuide/dev/v34/manifest/v2/keys_v2.json";
-    public static final String VERSION_MANIFEST_V2 = "https://raw.githubusercontent.com/FazziCLAY/SchoolGuide/dev/v34/manifest/v2/version_manifest_v2.json";
-    public static final String BUILTIN_SCHEDULE_V2 = "https://raw.githubusercontent.com/FazziCLAY/SchoolGuide/dev/v34/manifest/v2/builtin_schedule_v2.json";
+    public static final String KEYS_V2 = "https://raw.githubusercontent.com/FazziCLAY/SchoolGuide/main/manifest/v2/keys_v2.json";
+    public static final String VERSION_MANIFEST_V2 = "https://raw.githubusercontent.com/FazziCLAY/SchoolGuide/main/manifest/v2/version_manifest_v2.json";
+    public static final String BUILTIN_SCHEDULE_V2 = "https://raw.githubusercontent.com/FazziCLAY/SchoolGuide/main/manifest/v2/builtin_schedule_v2.json";
 
     /**
      * Схемы востановления DataFixer`a
