a4528298128dff41dd0c07a5fc02b66a4456d3ee
fazziclay <fazziclay@gmail.com>
2022-02-24 19:48:31 +0300

    uses MilkLog

--- app/src/main/java/ru/fazziclay/schoolguide/util/NetworkUtil.java
+++ app/src/main/java/ru/fazziclay/schoolguide/util/NetworkUtil.java
@@ -1,11 +1,11 @@
 package ru.fazziclay.schoolguide.util;
 
-import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+
+import ru.fazziclay.schoolguide.app.MilkLog;
 
 /**
  * Набор утилит для работы с сетью
@@ -17,7 +17,7 @@ public class NetworkUtil {
      * @return содержание страницы
      * **/
     public static String parseTextPage(String url) throws IOException {
-        if (DEBUG) Log.e("NETWORK_UTIL", "url=" + url);
+        if (DEBUG) MilkLog.g("NETWORK_UTIL (start) url=" + url);
         StringBuilder result = new StringBuilder();
         URL pageUrl = new URL(url);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(pageUrl.openStream()));
@@ -26,7 +26,7 @@ public class NetworkUtil {
             result.append(line);
         }
         bufferedReader.close();
-        if (DEBUG) Log.e("NETWORK_UTIL", "url=" + url + "; result=" + result);
+        if (DEBUG) MilkLog.g("NETWORK_UTIL: (end) url=" + url + "; result=" + result);
         return result.toString();
     }
 }
